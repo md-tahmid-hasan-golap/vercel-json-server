9f22f7b90aca012aa67fd4807d4fd9389e7ec3d0
md-tahmid-hasan-golap <golapraj47@gmail.com>
2025-08-08 00:53:52 +0600

    resolve

---
+++ firebase.js
@@ -0,0 +1,31 @@
+ 
+ 
+ 
+//  VITE_apiKey= AIzaSyAxqBkJBmExGS1gUvCIkZ8q3d2QUXKsN-8
+//   VITE_authDomain=prestice-project.firebaseapp.com
+//   VITE_projectId= prestice-project
+//   VITE_storageBucket= prestice-project.firebasestorage.app
+//   VITE_messagingSenderId= 518472264889
+//   VITE_appId= 1:518472264889:web:aaf9e66dc7a1ed2f22ff68
+
+
+
+
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_apiKey,
+//   authDomain: import.meta.env.VITE_authDomain,
+//   projectId: import.meta.env.VITE_projectId,
+//   storageBucket: import.meta.env.VITE_storageBucket,
+//   messagingSenderId: import.meta.env.VITE_messagingSenderId,
+//   appId: import.meta.env.VITE_appId
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// export default app;
